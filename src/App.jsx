09595f0e6002c06899d15d3fb80ca1b117d56666
Christopher Manzanares <cristoferm2013@gmail.com>
2022-08-08 19:13:18 -0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
   return (
     <BrowserRouter>
         <Routes>
+            <Route path="/" element={<p>Inicio</p>} />
             <Route path="/clientes" element={<Layout />}>
                 <Route index element={<Inicio />} />
                 <Route path="nuevo" element={<NuevoCliente />} />
